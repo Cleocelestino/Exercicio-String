3fbf90ebd8abb054a0bf039cbefc8465b2253d8f
Cleo Celestino <cleocelestinoo@gmail.com>
2020-02-27 23:33:34 -0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva seu nome");
+            string usuario = Console.ReadLine();
+            Console.WriteLine("Ola "+usuario+" seja bem vindo");
+
+      }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
